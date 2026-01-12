31bbd6e5d13c66180684ffd220b02c15fd50edfc
ely butabara <elybutabara@gmail.com>
2026-01-12 15:42:04 +0800

    fix for adding coaching time in course learners

--- app/Http/Controllers/Backend/CourseController.php
+++ app/Http/Controllers/Backend/CourseController.php
@@ -1057,7 +1057,7 @@ class CourseController extends Controller
             CoachingTimerManuscript::create([
                 'user_id' => $coursesTaken->user_id,
                 'file' => null,
-                'plan_type' => 1
+                'plan_type' => $request->coaching_time
             ]);
 
             CoachingTimerTaken::create([
